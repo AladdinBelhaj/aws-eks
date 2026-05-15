001a75358df2c3a61a0365d911ba3a8574952a4e
AladdinBelhaj <alabelhaj2002@gmail.com>
2026-05-15 13:02:07 +0100

    change backup s3 bucket name

--- backup.tf
+++ backup.tf
@@ -1,7 +1,7 @@
 terraform {
   # Terraform configuration for Azure backend.
   backend "s3" {
-    bucket       = "backup-bucket"
+    bucket       = "alaeddine-state-bucket"
     key          = "eks/alaeddine_belhaj/terraform.tfstate"
     region       = "eu-north-1"
     encrypt      = true
