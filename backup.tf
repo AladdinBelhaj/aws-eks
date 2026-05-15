bfa837af7d2b4f87e389bf025956efae682068a5
AladdinBelhaj <alabelhaj2002@gmail.com>
2026-05-15 13:16:47 +0100

    remove hard coded elements from backup file

--- backup.tf
+++ backup.tf
@@ -1,9 +1,9 @@
 terraform {
   # Terraform configuration for Azure backend.
   backend "s3" {
-    bucket       = "alaeddine-state-bucket"
-    key          = "eks/alaeddine_belhaj/terraform.tfstate"
-    region       = "eu-north-1"
+    bucket       = ""
+    key          = ""
+    region       = ""
     encrypt      = true
     use_lockfile = true
   }
